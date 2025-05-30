7cf770285826f030f0535a10af18fec446aab4ef
ÃIago Wilian <agowillian22@gmail.com>
2025-05-30 10:25:44 -0300

    deploy do projeto

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -5,6 +5,7 @@ import type Usuario from '../../models/Usuario'
 import { cadastrarUsuario } from '../../services/Service'
 import './Cadastro.css'
 import { RotatingLines } from 'react-loader-spinner'
+import { ToastAlerta } from '../../utils/ToastAlerta';
 
 function Cadastro() {
 
@@ -63,12 +64,12 @@ function Cadastro() {
           setUsuario
         );
 
-        alert('Usuário cadastrado com sucesso!')
+        ToastAlerta("Usuário cadastrado com sucesso!", "sucesso")
       } catch (error) {
-        alert('Erro ao cadastrar o usuário!')
+        ToastAlerta("Erro ao cadastrar o usuário!", "erro")
       }
     } else {
-      alert('Dados do usuário inconsistentes! Verifique as informações do cadastro.')
+      ToastAlerta("Dados do usuário inconsistentes! Verifique as informações do cadastro.", "info")
       setUsuario({ ...usuario, senha: '' })
       setConfirmaSenha('')
     }
